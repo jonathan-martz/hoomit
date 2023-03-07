696f8b24993cf2b549d9048ce416a979ef71c7d6
Jonathan Martz <git@jmartz.de>
2023-03-07 13:54:36 +0100

    add vuetify to main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,25 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+
 library.add(faBars)
 
 const pinia = createPinia()
 
 const app = createApp(App);
 app.use(pinia)
+app.use(vuetify)
 app.component('fa', FontAwesomeIcon)
 app.use(router)
 app.mount('#app')
